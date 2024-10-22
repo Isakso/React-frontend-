9ba2c5d1f170929726f92f5381929c77080fc0bd
Isaac <asabaisaac1@gmail.com>
2024-10-22 23:52:07 +0200

    created map of items

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,14 @@
-
 function ListGroup(){
 
-return (<ul className="list-group">
-  <li className="list-group-item">Todo</li>
-  <li className="list-group-item">A second item</li>
-  <li className="list-group-item">A third item</li>
-  <li className="list-group-item">A fourth item</li>
-  <li className="list-group-item">And a fifth one</li>
-</ul>);
-}
+const items = ["Todo1", "Todo2","Todo3"]
 
+ return(
+    <>
+     <h1>Todo List</h1>
+
+      (<ul className="list-group">
+        {items.map((items)=>(<li>{items}</li>))}
+        </ul>
+    </>);
+}
 export default ListGroup;
