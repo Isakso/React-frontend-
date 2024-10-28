d1bc2a29f129018306497b3eaffbdb005856253c
Isaac <asabaisaac1@gmail.com>
2024-10-28 19:48:04 +0100

    updated listGroup

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,19 @@
 function ListGroup(){
 
-const items = ["Todo1", "Todo2","Todo3"]
+let items = ["Todo1", "Todo2","Todo3"]
+
+
+
 
  return(
     <>
      <h1>Todo List</h1>
 
-      (<ul className="list-group">
-        {items.map((items)=>(<li>{items}</li>))}
+     {items.length === 0 && <p>No items Found </p> }
+    
+      <ul className="list-group">
+        {items.map((items)=>(
+          <li className="list-group-items" key = {items} onClick={()=>console.log('clicked')}> {items}</li>))}
         </ul>
     </>);
 }
